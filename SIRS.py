9a059c44baf2c43342410bd2ca1742ff32f10551
Salvadoor <salvadorrazatos610@gmail.com>
2025-07-28 16:09:33 -0600

    Added command line arguments instead of hardcoding files, so others can use

--- SIRS.py
+++ SIRS.py
@@ -5,21 +5,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from progress.bar import Bar
 
-data_file = open("/home/salvadoor/SFI Internship/SIR Models/medium_urbanization_data.txt", 'r').read()
-data_lists = data_file.split(']]')
+for i in range(11, len(sys.argv)):
+    data_file = open(sys.argv[i], 'r').read()
+    if ']]' in data_file:
+        data_lists = data_file.split(']]')
+        
+        data_lists =  '[' + ']], '.join(data_lists)[:-2].replace('None', '') + ']'
+        data_lists = eval(data_lists)
+    else:
+        data_file = open(sys.argv[i], 'r').read()
+        target_list = eval(data_file)
+        timestamp_list = [i for i in range(len(target_list))]
+        item_id_list = [0 for i in range(len(target_list))]
 
-data_lists =  '[' + ']], '.join(data_lists)[:-2].replace('None', '') + ']'
-data_lists = eval(data_lists)
+        full_data = zip(item_id_list, timestamp_list, target_list)
+        data_frame = pd.DataFrame(full_data, columns=["item_id", "timestamp", "target"])
 
-data_file = open("/home/salvadoor/SFI Internship/Real World Data Normalization/normalized_data.txt", 'r').read()
-target_list = eval(data_file)
-timestamp_list = [i for i in range(len(target_list))]
-item_id_list = [0 for i in range(len(target_list))]
-
-full_data = zip(item_id_list, timestamp_list, target_list)
-data_frame = pd.DataFrame(full_data, columns=["item_id", "timestamp", "target"])
-
-data_frame['target'] /= 2 * (10**6)
+        data_frame['target'] /= 2 * (10**6)
 # data_lists = [i / 30 for i in data_lists]
 
 # SIR model equations
@@ -50,37 +52,31 @@ bar = Bar('Running Simulation', max=len(data_lists))
 solution = odeint(SIRS_model, y0, t, args=(beta, gamma, theta, b_rate, d_rate))
 S, I, R = solution.T
 new_I = [0] + [(beta * s * i) / (s + i + r) for s, i, r in zip(S, I, R)]
+new_I = [i / sys.argv[10] for i in new_I]
 
-if eval(sys.argv[9]) == True:
-    for i in range(len(data_lists)):
-        for data_point in range(time_series_len):
-            mse.append((data_lists[i][data_point][-1] - new_I[data_point])**2)
 
-            #  time_series_data =  data_frame.loc[data_frame['item_id'] == i]
+for i in range(len(data_lists)):
+    for data_point in range(time_series_len):
+        mse.append((data_lists[i][data_point][-1] - new_I[data_point])**2)
+        
+    if i % 9 == 0:
+        plt.plot(t,  [data_point[-1] for data_point in data_lists[i]], label="Agent Based Output")
+        plt.plot(new_I, label="SIRS Output", color="orange")
+        """
+        plt.plot(S, label='Susceptible', color='blue')
+        plt.plot(I, label='Infected',  color='red')
+        plt.plot(R, label='Recovered', color='green')
+        plt.plot([S[i] + I[i] + R[i] for i in range(len(S))], label='Total', color='black')
+        """
+        plt.xlabel('Timesteps')
+        plt.ylabel('New Case Predictions')
+        plt.title('SIRS vs. Agent Based Model Results Medium Urbanization')
+        
+        plt.legend()
+        plt.show()
+    bar.next()
             
-        if i % 9 == 0:
-            plt.plot(t,  [data_point[-1] for data_point in data_lists[i]], label="Agent Based Output")
-            plt.plot(new_I, label="SIRS Output", color="orange")
-            """
-            plt.plot(S, label='Susceptible', color='blue')
-            plt.plot(I, label='Infected',  color='red')
-            plt.plot(R, label='Recovered', color='green')
-            plt.plot([S[i] + I[i] + R[i] for i in range(len(S))], label='Total', color='black')
-            """
-            plt.xlabel('Timesteps')
-            plt.ylabel('New Case Predictions')
-            plt.title('SIRS vs. Agent Based Model Results Medium Urbanization')
-                
-            plt.legend()
-            plt.show()
-        bar.next()
-            
-    bar.finish()
-else:
-    new_I = [i / 30 for i in new_I]
-    real_data_list = list(data_frame['target'])
-    for data_point in range(500):
-        mse.append((real_data_list[data_point] - new_I[data_point]) ** 2)
+bar.finish()
 
 
-print(sum(mse)/len(mse))
+print("Error:", sum(mse)/len(mse))
